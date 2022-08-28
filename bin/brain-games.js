977b9b45b78d66b5f80fd8170feec67dcedd3142
artu <carapina936@gmail.com>
2022-08-28 23:49:43 +0300

    step4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,3 @@
 import startTheGame from '../src/cli.js';
 
 startTheGame();
-
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default () => {  	
+export default () => {
   console.log('Welcome to the Brain Games!');
   console.log(`Hello, ${readlineSync.question('May I have your name? ')}!`);
 };
-
