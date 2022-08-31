0f5faad8cb88dd0ee5439f44d13e9e4858d2e64c
artu <carapina936@gmail.com>
2022-08-31 21:33:03 +0300

    point 3

--- src/games/b-gcd.js
+++ src/games/b-gcd.js
@@ -1,22 +1,14 @@
 import basisOfGames from '../index.js';
-import { getRandomNumber } from '../helpers.js';
+import { gcd, getRandomNumber } from '../helpers.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
 const getQuestionAndAnswer = () => {
-  let a = getRandomNumber(1, 100);
-  let b = getRandomNumber(1, 100);
+  const a = getRandomNumber(1, 100);
+  const b = getRandomNumber(1, 100);
   const question = `${a} ${b}`;
-
-  while (a !== b) {
-    if (a > b) {
-      a -= b;
-    } else {
-      b -= a;
-    }
-  }
-  const answer = String(a);
-  return [question, answer];
+  const answer = gcd(a, b);
+  return [question, String(answer)];
 };
 
 export default () => basisOfGames(description, getQuestionAndAnswer);
--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,15 @@
 const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
 const isEven = (number) => number % 2 === 0;
-
-export { getRandomNumber, isEven };
+const gcd = (num1, num2) => {
+  let a = num1;
+  let b = num2;
+  while (a !== b) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
+    }
+  }
+  return a;
+};
+export { getRandomNumber, isEven, gcd };
